5e100a2e857a266ae1a5efa75bd73e80a7fd247d
Octachore <octachore@outlook.com>
2016-09-02 22:54:56 +0200

    pawn can determined possible takings

--- Core/Katas/Draughts/Board.cs
+++ Core/Katas/Draughts/Board.cs
@@ -8,20 +8,53 @@ namespace Core.Katas.Draughts
     {
         public List<Piece> Pieces { get; set; } = new List<Piece>();
 
+        public void Add(Piece pawn) => Pieces.Add(pawn);
+
+        public void Add(IEnumerable<Piece> pawns) => Pieces.AddRange(pawns);
+
         public IEnumerable<Square> GetPossibleMoves(Piece piece)
         {
             int y = piece.Color == Color.Black ? piece.Y - 1 : piece.Y + 1;
             int x1 = piece.X - 1;
             int x2 = piece.X + 1;
 
-            if (CanMove(piece, x1, y)) yield return new Square(x1, y);
-            if (CanMove(piece, x2, y)) yield return new Square(x2, y);
+            if (IsFree(x1, y)) yield return new Square(x1, y);
+            if (IsFree(x2, y)) yield return new Square(x2, y);
         }
 
-        private bool CanMove(Piece piece, int x, int y) => x.In(0, 9) && y.In(0, 9) && Pieces.All(p => p?.Square != new Square(x, y));
+        public IEnumerable<Square> GetPossibleTakings(Piece piece)
+        {
+            Color enemyColor = piece.Color == Color.Black ? Color.White : Color.Black;
 
-        public void Add(Piece pawn) => Pieces.Add(pawn);
+            int enemyY = piece.Color == Color.Black ? piece.Y - 1 : piece.Y + 1;
+            int enemyX1 = piece.X - 1;
+            int enemyX2 = piece.X + 1;
 
-        public void Add(IEnumerable<Piece> pawns) => Pieces.AddRange(pawns);
+            int destinationY = piece.Color == Color.Black ? enemyY - 1 : enemyY + 1;
+            int destinationX1 = enemyX1 - 1;
+            int destinationX2 = enemyX2 + 1;
+
+            if (ContainsPawn(enemyX1, enemyY, enemyColor) && IsFree(destinationX1, destinationY))
+            {
+                ////TakePiece(destinationX1, destinationY);
+                yield return new Square(destinationX1, destinationY);
+            }
+
+            if (ContainsPawn(enemyX2, enemyY, enemyColor) && IsFree(destinationX2, destinationY))
+            {
+                ////TakePiece(destinationX2, destinationY);
+                yield return new Square(destinationX2, destinationY);
+            }
+        }
+
+        private void TakePiece(int x, int y)
+        {
+            Piece piece = Pieces.SingleOrDefault(p => p.Square == new Square(x, y));
+            if (piece != null) Pieces.Remove(piece);
+        }
+
+        private bool ContainsPawn(int x, int y, Color? color = null) => x.In(0, 9) && y.In(0, 9) && Pieces.Where(p => p.Square == new Square(x, y)).Any(p => (color == null) || (p.Color == color.Value));
+
+        private bool IsFree(int x, int y) => x.In(0, 9) && y.In(0, 9) && Pieces.All(p => p?.Square != new Square(x, y));
     }
 }
--- Tests/Katas/DraughtsTests.cs
+++ Tests/Katas/DraughtsTests.cs
@@ -5,48 +5,86 @@ namespace Tests.Katas
 {
     public class DraughtsTests
     {
+        private Board _board;
+
+        private Piece _pw1;
+        private Piece _pw2;
+        private Piece _pw3;
+        private Piece _pw4;
+
+        private Piece _pb1;
+        private Piece _pb2;
+        private Piece _pb3;
+        private Piece _pb4;
+
+        [SetUp]
+        public void Setup()
+        {
+            _board = new Board();
+
+            _pw1 = new Piece(1, 1, Color.White);
+            _pw2 = new Piece(3, 9, Color.White);
+            _pw3 = new Piece(3, 5, Color.White);
+            _pw4 = new Piece(9, 0, Color.White);
+
+            _pb1 = new Piece(2, 2, Color.Black);
+            _pb2 = new Piece(5, 1, Color.Black);
+            _pb3 = new Piece(6, 6, Color.Black);
+            _pb4 = new Piece(4, 4, Color.Black);
+
+            var whitePawns = new[] { _pw1, _pw2, _pw3, _pw4 };
+            var blackPawns = new[] { _pb1, _pb2, _pb3, _pb4 };
+
+            _board.Add(whitePawns);
+            _board.Add(blackPawns);
+        }
+
         [Test]
         public void A_Pawn_Can_Detremine_Its_Possible_Moves_Without_Taking_On_A_Board()
         {
-            var board = new Board();
-
-
-            var pw1 = new Piece(1, 1, Color.White);
-            var pw2 = new Piece(3, 9, Color.White);
-            var pw3 = new Piece(3, 5, Color.White);
-            var pw4 = new Piece(0, 0, Color.White);
-
-            var pb1 = new Piece(2, 2, Color.Black);
-            var pb2 = new Piece(5, 1, Color.Black);
-            var pb3 = new Piece(6, 6, Color.Black);
-            var pb4 = new Piece(4, 4, Color.Black);
-
-            var whitePawns = new[] { pw1, pw2, pw3, pw4 };
-            var blackPawns = new[] { pb1, pb2, pb3, pb4 };
-
-            board.Add(whitePawns);
-            board.Add(blackPawns);
-
-
             var pw1Mouves = new[] { new Square(0, 2) };
             var pw2Mouves = new Square[0];
             var pw3Mouves = new[] { new Square(2, 6), new Square(4, 6) };
-            var pw4Mouves = new Square[0];
+            var pw4Mouves = new[] { new Square(8, 1) };
 
             var pb1Mouves = new[] { new Square(3, 1) };
             var pb2Mouves = new[] { new Square(4, 0), new Square(6, 0) };
             var pb3Mouves = new[] { new Square(5, 5), new Square(7, 5) };
             var pb4Mouves = new[] { new Square(3, 3), new Square(5, 3) };
 
-            Assert.That(board.GetPossibleMoves(pw1), Is.EquivalentTo(pw1Mouves));
-            Assert.That(board.GetPossibleMoves(pw2), Is.EquivalentTo(pw2Mouves));
-            Assert.That(board.GetPossibleMoves(pw3), Is.EquivalentTo(pw3Mouves));
-            Assert.That(board.GetPossibleMoves(pw4), Is.EquivalentTo(pw4Mouves));
+            Assert.That(_board.GetPossibleMoves(_pw1), Is.EquivalentTo(pw1Mouves));
+            Assert.That(_board.GetPossibleMoves(_pw2), Is.EquivalentTo(pw2Mouves));
+            Assert.That(_board.GetPossibleMoves(_pw3), Is.EquivalentTo(pw3Mouves));
+            Assert.That(_board.GetPossibleMoves(_pw4), Is.EquivalentTo(pw4Mouves));
 
-            Assert.That(board.GetPossibleMoves(pb1), Is.EquivalentTo(pb1Mouves));
-            Assert.That(board.GetPossibleMoves(pb2), Is.EquivalentTo(pb2Mouves));
-            Assert.That(board.GetPossibleMoves(pb3), Is.EquivalentTo(pb3Mouves));
-            Assert.That(board.GetPossibleMoves(pb4), Is.EquivalentTo(pb4Mouves));
+            Assert.That(_board.GetPossibleMoves(_pb1), Is.EquivalentTo(pb1Mouves));
+            Assert.That(_board.GetPossibleMoves(_pb2), Is.EquivalentTo(pb2Mouves));
+            Assert.That(_board.GetPossibleMoves(_pb3), Is.EquivalentTo(pb3Mouves));
+            Assert.That(_board.GetPossibleMoves(_pb4), Is.EquivalentTo(pb4Mouves));
+        }
+
+        [Test]
+        public void A_Pawn_Can_Determine_Its_Possible_Takings()
+        {
+            var pw1Takings = new[] { new Square(3, 3) };
+            var pw2Takings = new Square[0];
+            var pw3Takings = new Square[0];
+            var pw4Takings = new Square[0];
+
+            var pb1Takings = new[] { new Square(0, 0) };
+            var pb2Takings = new Square[0];
+            var pb3Takings = new Square[0];
+            var pb4Takings = new Square[0];
+
+            Assert.That(_board.GetPossibleTakings(_pw1), Is.EquivalentTo(pw1Takings));
+            Assert.That(_board.GetPossibleTakings(_pw2), Is.EquivalentTo(pw2Takings));
+            Assert.That(_board.GetPossibleTakings(_pw3), Is.EquivalentTo(pw3Takings));
+            Assert.That(_board.GetPossibleTakings(_pw4), Is.EquivalentTo(pw4Takings));
+
+            Assert.That(_board.GetPossibleTakings(_pb1), Is.EquivalentTo(pb1Takings));
+            Assert.That(_board.GetPossibleTakings(_pb2), Is.EquivalentTo(pb2Takings));
+            Assert.That(_board.GetPossibleTakings(_pb3), Is.EquivalentTo(pb3Takings));
+            Assert.That(_board.GetPossibleTakings(_pb4), Is.EquivalentTo(pb4Takings));
         }
     }
 }
